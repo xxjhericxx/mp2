f4fe09d93f5eaed2d5d17332abf2897077dede79
xxjhericxx <icloudstrife07@gmail.com>
2023-07-11 12:55:29 +0800

    adding dependencies

---
+++ backend/server.js
@@ -0,0 +1,17 @@
+// entry point
+const express = require(`express`);
+
+const app = express();
+
+// configure server
+app.use(express.json());
+app.use(express.urlencoded());
+
+// routes
+
+
+const PORT = 8000;
+
+app.listen(PORT, () => {
+    console.log(`Server is currrently running on port ${PORT}`);
+})
