b215957f3f5c47057245a4d337c6d7a56357142d
Jomell <Jomell>
2023-07-11 19:49:20 +0800

    Added Template

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Nav from './nav'
+import Rout from './rout';
+import {BrowserRouter} from 'react-router-dom';
+import Footer from './footer';
+import Productdetail from './productdetail';
+const App = () => {
+  // add to cart
+  const [cart, setCart] = useState([])
+  //product Detail
+  const [close, setClose] = useState(false)
+  const [detail, setDetail] = useState([])
+  //filter product
+  const [product, setProduct] = useState(Productdetail)
+  const searchbtn = (product) => 
+  {
+    const change = Productdetail.filter((x) => 
+    {
+      return x.Cat === product
+    })
+    setProduct(change)
+  }
+  //product detail
+  const view = (product) => 
+  {
+    setDetail([{...product}])
+    setClose(true)
+  }
 
-function App() {
+  // add to cart
+  const addtocart = (product) => 
+  {
+    const exsit = cart.find((x) => 
+    {
+      return x.id === product.id
+    })
+    if(exsit)
+    {
+      alert("This Product is already added to cart")
+    }
+    else
+    { 
+      setCart([...cart, {...product, qty:1}])
+      alert("product is added to cart")
+    }
+  } 
+  console.log(cart)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <BrowserRouter>
+    <Nav searchbtn={searchbtn}/>
+    <Rout product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} cart={cart} setCart={setCart} addtocart={addtocart}/>
+    <Footer />
+    </BrowserRouter>
+    </>
+  )
 }
 
-export default App;
+export default App
