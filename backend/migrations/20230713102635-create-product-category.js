2d1aa7fc392aa41bc75beb289bfda6ce13a94d87
xxjhericxx <icloudstrife07@gmail.com>
2023-07-13 18:34:33 +0800

    add product category table

---
+++ backend/migrations/20230713102635-create-product-category.js
@@ -0,0 +1,37 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('ProductCategories', {
+      productCategoryId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productCategoryTitle: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      productCategoryDescription: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      productCategoryQuantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('ProductCategories');
+  }
+};
