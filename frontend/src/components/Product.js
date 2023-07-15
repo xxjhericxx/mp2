7a5f0bb5e33c577c00cc712ebfe054d5035c0c4d
Jomell <Jomell>
2023-07-15 23:00:32 +0800

    Finished Products With Details for Home

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -30,14 +30,21 @@ const Product = ({ product }) => {
             </div>
           </button>
           <Link
-            to={'/'}
+            to={`/product/${id}`}
             className='w-12 h-12 bg-white flex justify-center items-center text-primary drop-shadow-xl'
           >
             <BsEyeFill />
           </Link>
         </div>
       </div>
-      <div>2</div>
+      {/* category & title & price */}
+      <div>
+        <div className='text-sm capitalize text-gray-500 mb-1'>{category}</div>
+        <Link to ={`/product/${id}`}>
+          <h2 className='font-semibold mb-1'>{title}</h2>
+        </Link>
+        <div className='font-semibold'>$ {price}</div>
+      </div>
     </div>
   );
 };
