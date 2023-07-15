c1a71d4549403a2b557439d92a0b09efca49afa1
Jomell <Jomell>
2023-07-15 21:15:39 +0800

    Connected to FakeStore API for Development

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,59 +1,31 @@
-import React, {useState} from 'react'
-import Nav from './nav'
-import Rout from './rout';
-import {BrowserRouter} from 'react-router-dom';
-import Footer from './footer';
-import Productdetail from './productdetail';
+import React from 'react';
+
+// import react router dom
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// import pages
+import Home from './pages/Home';
+import ProductDetails from './pages/ProductDetails';
+
+// import components
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
-  // add to cart
-  const [cart, setCart] = useState([])
-  //product Detail
-  const [close, setClose] = useState(false)
-  const [detail, setDetail] = useState([])
-  //filter product
-  const [product, setProduct] = useState(Productdetail)
-  const searchbtn = (product) => 
-  {
-    const change = Productdetail.filter((x) => 
-    {
-      return x.Cat === product
-    })
-    setProduct(change)
-  }
-  //product detail
-  const view = (product) => 
-  {
-    setDetail([{...product}])
-    setClose(true)
-  }
-
-  // add to cart
-  const addtocart = (product) => 
-  {
-    const exsit = cart.find((x) => 
-    {
-      return x.id === product.id
-    })
-    if(exsit)
-    {
-      alert("This Product is already added to cart")
-    }
-    else
-    { 
-      setCart([...cart, {...product, qty:1}])
-      alert("product is added to cart")
-    }
-  } 
-  console.log(cart)
   return (
-    <>
-    <BrowserRouter>
-    <Nav searchbtn={searchbtn}/>
-    <Rout product={product} setProduct={setProduct} detail={detail} view={view} close={close} setClose={setClose} cart={cart} setCart={setCart} addtocart={addtocart}/>
-    <Footer />
-    </BrowserRouter>
-    </>
-  )
-}
+  <div className='overflow-hidden'>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/product/:id' element={<ProductDetails />} />
+      </Routes>
+      <Sidebar />
+      <Footer />
+    </Router>
+  </div>
+  );
+};
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Auth0Provider } from '@auth0/auth0-react';
+// product provider
+import ProductProvider from './contexts/ProductContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-    domain="dev-8p6c8g3ktchsc5w8.us.auth0.com"
-    clientId="lZE1bD0jYoqnz2A66PhEBxujvByVZkln"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
-    <App />
-  </Auth0Provider>  
+  <ProductProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ProductProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
