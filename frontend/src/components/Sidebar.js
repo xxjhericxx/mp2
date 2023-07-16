69ae1d316309be631115582ee07877c3b215ba6d
Jomell <Jomell>
2023-07-17 01:48:34 +0800

    Finished Cart Context

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -8,9 +8,12 @@ import { FiTrash2 } from 'react-icons/fi';
 import CartItem from '../components/CartItem';
 // import sidebar context
 import { SidebarContext } from '../contexts/SidebarContext';
+// import cart context
+import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
+  const {cart} = useContext(CartContext);
   return (
     <div
       className={`${ isOpen ? 'right-0' : '-right-full'} 
@@ -26,6 +29,11 @@ const Sidebar = () => {
         >
           <IoMdArrowForward className='text-2xl' />
         </div>
+      </div>
+      <div>
+        {cart.map((item) => {
+        return <CartItem item={item} key={item.id} />;
+      })}
       </div>
     </div>
   );
--- frontend/src/contexts/CartContext.js
+++ frontend/src/contexts/CartContext.js
@@ -1,7 +1,39 @@
-import React from 'react';
+import React, { createContext, useState, useEffect } from 'react';
+// create context
+export const CartContext = createContext();
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+const CartProvider = ({ children }) => {
+  // cart state
+  const [cart,setCart] = useState([]);
+
+  // add to cart
+  const addToCart = (product, id) => {
+    const newItem = { ...product, amount: 1 };
+    // check for item in cart
+    const cartItem = cart.find((item) => {
+      return item.id === id;
+    });
+    // if cart item is in cart
+    if (cartItem) {
+      const newCart = [...cart].map(item => {
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount + 1};
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem]);
+    }
+  };
+  console.log(cart);
+
+  return (
+    <CartContext.Provider value={{ cart, addToCart }}>
+      {children}
+    </CartContext.Provider>
+  );
 };
 
-export default CartContext;
+export default CartProvider;
