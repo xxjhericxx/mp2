9b7368fb375867b2c8a951bd6ad2c411807c2134
Jomell <Jomell>
2023-07-17 04:13:13 +0800

    Finished Cart Item Function

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -13,7 +13,7 @@ import { CartContext } from '../contexts/CartContext';
 
 const Sidebar = () => {
   const { isOpen, handleClose } = useContext(SidebarContext);
-  const {cart} = useContext(CartContext);
+  const {cart, clearCart, total } = useContext(CartContext);
   return (
     <div
       className={`${ isOpen ? 'right-0' : '-right-full'} 
@@ -30,10 +30,27 @@ const Sidebar = () => {
           <IoMdArrowForward className='text-2xl' />
         </div>
       </div>
-      <div>
+      <div className='flex flex-col gap-y-2 h-[520px] lg:h-[640px] overflow-y-auto 
+      overflow-x-hidden border-b'>
         {cart.map((item) => {
         return <CartItem item={item} key={item.id} />;
       })}
+      </div>
+      <div className='flex flex-col gap-y-3 py-4 mt-4'>
+        <div className='flex w-full justify-between items-center'>
+          {/* total */}
+          <div className='uppercase font-semibold'>
+            <span className='mr-2'>Total:</span>$ {parseFloat(total).toFixed(2)}
+          </div>
+          {/* clear cart icon */}
+          <div 
+          onClick={clearCart} 
+          className='cursor-pointer py-4 bg-red-500 text-white w-12 h-12 flex 
+          justify-center items-center text-xl'
+          >
+            <FiTrash2 />
+          </div>
+        </div>
       </div>
     </div>
   );
