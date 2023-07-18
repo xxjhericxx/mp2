4e10371d76c5341494a9be40ddf694e3ee3e08a7
xxjhericxx <icloudstrife07@gmail.com>
2023-07-18 09:56:19 +0800

    add productPrice on customer order model

--- backend/migrations/20230713105104-create-customer-order.js
+++ backend/migrations/20230713105104-create-customer-order.js
@@ -18,6 +18,10 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      productPrice: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
       orderQuantity: {
         type: Sequelize.INTEGER,
         allowNull: false
