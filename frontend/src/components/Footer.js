3709bca59b7612fd61d74dc994fa7719ed7edf9a
Jomell <Jomell>
2023-07-20 20:16:57 +0800

    Completed Frontend UI for clientside

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer className='bg-primary py-12'>
       <div className='container mx-auto'>
         <p className='text-white text-center'>
-          Copyright &copy; Ecommerce Shop 2022. All rights reserved.
+          Copyright &copy; Ecommerce Shop 2023. All rights reserved.
         </p>
       </div>
     </footer>
